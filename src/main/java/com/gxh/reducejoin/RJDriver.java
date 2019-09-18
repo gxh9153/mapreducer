7a6ad8121996e0a24f48b1e9afb008b10bf4bf74
gxh <1320546563@qq.com>
2019-09-18 10:44:03 +0800

    reducer join

---
+++ src/main/java/com/gxh/reducejoin/RJDriver.java
@@ -0,0 +1,36 @@
+package com.gxh.reducejoin;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class RJDriver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Job job = Job.getInstance(new Configuration());
+
+        job.setJarByClass(RJDriver.class);
+
+        job.setMapperClass(RJMapper.class);
+        job.setReducerClass(RJReducer.class);
+
+        job.setMapOutputKeyClass(OrderBean.class);
+        job.setMapOutputValueClass(NullWritable.class);
+
+        job.setOutputKeyClass(OrderBean.class);
+        job.setOutputValueClass(NullWritable.class);
+
+        job.setGroupingComparatorClass(RJComparator.class);
+
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+
+        boolean b = job.waitForCompletion(true);
+
+        System.exit(b? 0:1);
+    }
+}
